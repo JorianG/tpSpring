a7c0c3f48cf50cc2646cb4a4ee830828731e0aa7
JorianG <joriangouagout1@gmail.com>
2024-12-06 14:33:27 +0100

    remove unused imports

--- src/main/java/dev/joriang/tpspringboot/TpSpringBootApplication.java
+++ src/main/java/dev/joriang/tpspringboot/TpSpringBootApplication.java
@@ -1,13 +1,20 @@
 package dev.joriang.tpspringboot;
 
+import dev.joriang.tpspringboot.entities.Roles;
+import dev.joriang.tpspringboot.entities.User;
+import dev.joriang.tpspringboot.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class TpSpringBootApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(TpSpringBootApplication.class, args);
@@ -17,4 +24,5 @@ public class TpSpringBootApplication {
     public String bonjour() {
         return "Hello World !";
     }
+
 }
